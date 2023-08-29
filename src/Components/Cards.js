bf5ee517426d174263a6fb62009dbaead4ddf482
samuelondo <135150972+samuelondo@users.noreply.github.com>
2023-08-29 07:56:07 -0500

    Update Cards.js

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -5,6 +5,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Imagen from './Assets/zzzzz.jpeg';
 
 const bull = (
   <Box
@@ -44,29 +45,7 @@ export default function BasicCard() {
 
 
 
-    <div className='card'>
-         <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions className='boton'>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    </div>
+    <img className= 'img' src={Imagen}></img>
 
 
     </div>
